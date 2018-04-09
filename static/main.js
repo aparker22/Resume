a8e1d22fc3baa42ceeb0e95f0be88ba035849294
Ashley Parker <aparker7698@att.net>
2018-04-09 16:19:44 -0400

    Added mobile responsiveness

--- static/main.js
+++ static/main.js
@@ -4,6 +4,9 @@ let addEventListeners = () => {
 
     let form = document.querySelector('body > div > div.contact > div.contact-form > form')
     form.addEventListener('submit', sendEmail);
+
+    let mobileMenu = document.querySelector('body > div > div.top > div.top-navbar-mobile > ul > li:nth-child(1)');
+    mobileMenu.addEventListener('click', openMobileMenu);
 }
 
 let addDownloadResumeMenu = (e) => {
@@ -19,11 +22,18 @@ let sendEmail = (e) => {
     let form = document.querySelector('body > div > div.contact > div.contact-form > form')
     let email = {email: form.email.value, subject: form.subject.value, message: form.message.value};
     console.log('Message Sent');
+    console.log(`${window.location}/email`)
     form.reset();
-    fetch('/email', {
+    fetch(`${window.location}/email`, {
         method: 'POST', 
         body: JSON.stringify(email),
       })
+}
+
+let openMobileMenu = (e) => {
+    e.preventDefault();
+    let mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('active');
 }
 
 addEventListeners();
